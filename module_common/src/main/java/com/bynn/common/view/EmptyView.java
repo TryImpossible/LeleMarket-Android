dc1db8404b142078fae02a81ec03352c897c1ad6
Barry <865068275@qq.com>
2019-12-26 22:01:30 +0800

    EmptyView控件

---
+++ module_common/src/main/java/com/bynn/common/view/EmptyView.java
@@ -0,0 +1,33 @@
+package com.bynn.common.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class EmptyView extends LinearLayout {
+    public EmptyView(Context context) {
+        this(context, null);
+    }
+
+    public EmptyView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public EmptyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public EmptyView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+
+    }
+}
